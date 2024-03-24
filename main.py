5f537f1e7db1aac14db007870edeac78682dae2b
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2024-03-24 18:04:50 -0400

    Add new_file() feature - Add functionality to start a new file from the Menu - Add confirm_save() to check if file must be saved before performing an action - Change window title to variable to make it easier to change while the program is running

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ from pathlib import Path
 class MainWindow:
     def __init__(self, parent):
         self.parent = parent
+        self.APP_TITLE = "Piper Read Aloud"
         self.setup_gui()
     
     def setup_gui(self):
-        self.parent.title("Piper Read Aloud")
+        self.parent.title(self.APP_TITLE)
         self.menubar = tk.Menu(self.parent)
         self.parent.config(menu=self.menubar)
         self.file_menu = tk.Menu(self.menubar, tearoff=0)
-        self.file_menu.add_command(label="New file...")
+        self.file_menu.add_command(label="New file...", command=self.new_file)
         self.file_menu.add_command(label="Open file...", command=self.open_file)
         self.file_menu.add_command(label="Save file...", command=self.save_file)
         self.file_menu.add_command(label="Exit", command=self.parent.destroy)
@@ -35,7 +36,7 @@ class MainWindow:
         if not filepath:
             return
         
-        self.parent.title(f"Piper Read Aloud - {Path(filepath).name}")
+        self.parent.title(f"{self.APP_TITLE} - {Path(filepath).name}")
         with open(filepath) as file:
             file_content = file.read()
 
@@ -57,12 +58,28 @@ class MainWindow:
                 file.write(text_content)
             if Path(filepath).exists():
                 messagebox.showinfo("File saved successfully", f"File saved successfully on {filepath}")
+                self.parent.title = f"{self.APP_TITLE} - {Path(filepath).name}"
         except Exception as e:
             messagebox.showerror("Error", f"Error: {e}")
 
+    def confirm_save(self):
+        text_content = self.text_entry.get("1.0", "end-1c")
+        if not text_content:
+            return False
+        answer = messagebox.askyesnocancel("Continue?", "This will close the current file. If you haven't saved its contents, loss of data could happen. \nDo you want to save the file?")
+        return answer
+
 
     def new_file(self):
-        pass
+        save_file = self.confirm_save()
+        if save_file:
+            self.save_file()
+        elif save_file == None:
+            return
+        
+        self.text_entry.delete("1.0", tk.END)
+        self.parent.title = self.APP_TITLE
+        
 
 if __name__ == "__main__":
     root = tk.Tk()

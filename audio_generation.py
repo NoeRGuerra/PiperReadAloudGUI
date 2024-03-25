cb3ca84dcf109a5947de06f2533c9057ee0664bc
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2024-03-25 06:03:10 -0400

    Format code using Black
    Remove unnecesary import from main.py

--- audio_generation.py
+++ audio_generation.py
@@ -3,16 +3,20 @@ import wave
 import numpy as np
 import sounddevice as sd
 
+
 def generate_audio(text, model, output_filepath):
     model_filepath = f"models/{model}.onnx"
     voice = PiperVoice.load(model_filepath)
-    with wave.open(output_filepath, 'w') as wav_file:
+    with wave.open(output_filepath, "w") as wav_file:
         voice.synthesize(text, wav_file)
+
 
 def stream_audio(text, model):
     model_filepath = f"models/{model}.onnx"
     voice = PiperVoice.load(model_filepath)
-    stream = sd.OutputStream(samplerate=voice.config.sample_rate, channels=1, dtype='int16')
+    stream = sd.OutputStream(
+        samplerate=voice.config.sample_rate, channels=1, dtype="int16"
+    )
     stream.start()
     for audio_bytes in voice.synthesize_stream_raw(text):
         int_data = np.frombuffer(audio_bytes, dtype=np.int16)
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from tkinter import ttk
-from piper.voice import PiperVoice
 from pathlib import Path
 from audio_generation import generate_audio, stream_audio
+
 
 class MainWindow:
     def __init__(self, parent):
         self.parent = parent
         self.APP_TITLE = "Piper Read Aloud"
         self.setup_gui()
-    
+
     def setup_gui(self):
         self.parent.title(self.APP_TITLE)
         self.menubar = tk.Menu(self.parent)
@@ -26,48 +26,63 @@ class MainWindow:
         self.text_entry.grid(row=0, column=0, columnspan=2, sticky="NSEW")
         self.model_dropdown = ttk.Combobox(self.parent, width=10)
         self.model_dropdown.grid(row=1, column=0, sticky="NSEW")
-        self.generate_btn = ttk.Button(self.parent, text="Generate", width=10, command=self.generate_audio)
+        self.generate_btn = ttk.Button(
+            self.parent, text="Generate", width=10, command=self.generate_audio
+        )
         self.generate_btn.grid(row=1, column=1, sticky="NSEW")
-        self.stream_btn = ttk.Button(self.parent, text="Stream", command=self.stream_audio)
+        self.stream_btn = ttk.Button(
+            self.parent, text="Stream", command=self.stream_audio
+        )
         self.stream_btn.grid(row=2, column=0, sticky="NSEW")
         self.build_dropdown()
         self.parent.rowconfigure(0, weight=1)
         self.parent.columnconfigure(0, weight=1)
         self.parent.columnconfigure(1, weight=1)
-    
+
     def build_dropdown(self):
         models = Path("models/").glob("*.onnx")
-        available_models = [Path(i).stem for i in models if Path(i).with_suffix(".onnx.json").exists()]
-        self.model_dropdown['values'] = available_models
+        available_models = [
+            Path(i).stem for i in models if Path(i).with_suffix(".onnx.json").exists()
+        ]
+        self.model_dropdown["values"] = available_models
         self.model_dropdown.current(0)
 
     def open_file(self):
-        filepath = filedialog.askopenfilename(parent=self.parent, filetypes=[("Text files", "*.txt")])
+        filepath = filedialog.askopenfilename(
+            parent=self.parent, filetypes=[("Text files", "*.txt")]
+        )
         if not filepath:
             return
-        
+
         self.parent.title(f"{self.APP_TITLE} - {Path(filepath).name}")
         with open(filepath) as file:
             file_content = file.read()
 
         if self.text_entry.get("1.0", "end-1c"):
-            answer = messagebox.askyesno("Do you want to proceed?", "Opening a new file will overwrite the curent text. Do you want to proceed?")
+            answer = messagebox.askyesno(
+                "Do you want to proceed?",
+                "Opening a new file will overwrite the curent text. Do you want to proceed?",
+            )
             if not answer:
                 return
-            
+
         self.text_entry.delete("1.0", tk.END)
         self.text_entry.insert("1.0", file_content)
 
     def save_file(self):
-        filepath = filedialog.asksaveasfilename(parent=self.parent, filetypes=[("Text files", "*.txt")])
+        filepath = filedialog.asksaveasfilename(
+            parent=self.parent, filetypes=[("Text files", "*.txt")]
+        )
         if not filepath:
             return
         text_content = self.text_entry.get("1.0", "end-1c")
         try:
-            with open(filepath, 'w') as file:
+            with open(filepath, "w") as file:
                 file.write(text_content)
             if Path(filepath).exists():
-                messagebox.showinfo("File saved successfully", f"File saved successfully at {filepath}")
+                messagebox.showinfo(
+                    "File saved successfully", f"File saved successfully at {filepath}"
+                )
                 self.parent.title = f"{self.APP_TITLE} - {Path(filepath).name}"
         except Exception as e:
             messagebox.showerror("Error", f"Error: {e}")
@@ -76,9 +91,11 @@ class MainWindow:
         text_content = self.text_entry.get("1.0", "end-1c")
         if not text_content:
             return False
-        answer = messagebox.askyesnocancel("Continue?", "This will close the current text. If you haven't saved it, loss of data could happen. \nDo you want to save the text file?")
+        answer = messagebox.askyesnocancel(
+            "Continue?",
+            "This will close the current text. If you haven't saved it, loss of data could happen. \nDo you want to save the text file?",
+        )
         return answer
-
 
     def new_file(self):
         save_file = self.confirm_save()
@@ -86,10 +103,10 @@ class MainWindow:
             self.save_file()
         elif save_file == None:
             return
-        
+
         self.text_entry.delete("1.0", tk.END)
         self.parent.title = self.APP_TITLE
-    
+
     def exit(self):
         save_file = self.confirm_save()
         if save_file:
@@ -103,10 +120,14 @@ class MainWindow:
         if not text_content:
             messagebox.showwarning("Error", "Enter some text to generate audio")
             return
-        filepath = filedialog.asksaveasfilename(defaultextension=".wav", filetypes=[("Wave files", "*.wav")])
+        filepath = filedialog.asksaveasfilename(
+            defaultextension=".wav", filetypes=[("Wave files", "*.wav")]
+        )
         generate_audio(text_content, self.model_dropdown.get(), filepath)
         if Path(filepath).exists():
-            messagebox.showinfo("File saved successfully", f"File saved successfully at {filepath}")
+            messagebox.showinfo(
+                "File saved successfully", f"File saved successfully at {filepath}"
+            )
         else:
             messagebox.showinfo("Error", "Error")
 

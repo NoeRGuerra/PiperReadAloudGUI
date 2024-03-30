d69a42050d50119006df82499872988fafd4f827
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2024-03-30 12:20:25 -0400

    Improve audio model path search in audio functions
    - Add find_model_path() to look for model files recursively inside of 'models/' directory, fixing error that would prevent the program to find files located within subdirectories

--- audio_generation.py
+++ audio_generation.py
@@ -6,14 +6,14 @@ import sounddevice as sd
 
 
 def generate_audio(text, model, output_filepath):
-    model_filepath = f"models/{model}.onnx"
+    model_filepath = find_model_path(model)
     voice = PiperVoice.load(model_filepath)
     with wave.open(output_filepath, "w") as wav_file:
         voice.synthesize(text, wav_file)
 
 
 def stream_audio(text, model):
-    model_filepath = f"models/{model}.onnx"
+    model_filepath = find_model_path(model)
     voice = PiperVoice.load(model_filepath)
     stream = sd.OutputStream(
         samplerate=voice.config.sample_rate, channels=1, dtype="int16"
@@ -32,3 +32,10 @@ def list_models():
         Path(i).stem for i in models if Path(i).with_suffix(".onnx.json").exists()
     ]
     return available_models
+
+def find_model_path(model_name):
+    models = Path("models/").rglob("*.onnx")
+    for model in models:
+        if model.stem == model_name:
+            return str(model)
+    return None
